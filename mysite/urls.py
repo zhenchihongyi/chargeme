df4de506d45a95021fd09255f98691da09f0b5be
tiger <qi.zhang@aurora-lab.co.jp>
2018-04-11 10:42:05 +0900

    Added views to create/edit blog post inside the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -12,6 +12,7 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    ssss
 """
 from django.conf.urls import include, url
 from django.contrib import admin
