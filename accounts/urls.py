54d4b5813eebb7aaa13cf0a6a0c5c4f24cb6b54e
tiger <qi.zhang@aurora-lab.co.jp>
2018-04-12 16:47:28 +0900

    Added user create view to the website.

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login$', auth_views.login, {'template_name': 'accounts/login.html'}, name='login'),
     url(r'^logout$', auth_views.logout, name='logout'),
+    url(r'^new$', views.new, name='new'),
+    url(r'^create$', views.create, name='create'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,23 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
+
 
 @login_required
 def index(request):
     return render(request, 'accounts/index.html')
+
+def new(request):
+    form = UserCreationForm()
+    return render(request, 'accounts/new.html', {'form': form,})
+
+def create(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('./login')
+        return render(request, 'accounts/new.html', {'form': form,})
+    else:
+        raise Http404
