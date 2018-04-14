8c20dd44064ad8869f6c08da9882a1b995c89db2
tiger <qi.zhang@aurora-lab.co.jp>
2018-04-14 15:05:13 +0900

    Added user create(email) view to the website.

--- accounts/views.py
+++ accounts/views.py
@@ -2,22 +2,26 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
-
+from . import forms
 
 @login_required
 def index(request):
     return render(request, 'accounts/index.html')
 
 def new(request):
-    form = UserCreationForm()
+#    form=UserCreationForm()
+    form = forms.UserCreateForm()
     return render(request, 'accounts/new.html', {'form': form,})
 
 def create(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+#        form = UserCreationForm(request.POST)]
+        form = forms.UserCreateForm(request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('./login')
         return render(request, 'accounts/new.html', {'form': form,})
     else:
         raise Http404
+
+
